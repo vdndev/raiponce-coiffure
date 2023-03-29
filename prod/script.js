5bcc53ea570894402dfcb77620988e455aecfeee
BahiaL <bahlarcade@hotmail.com>
2023-03-29 13:32:45 +0200

    changement fonction slide

--- prod/script.js
+++ prod/script.js
@@ -1,26 +1,48 @@
-var slideIndex = 0;
-var slides = document.querySelectorAll(".slides");
+let slideIndex = 0;
+let conteneur = document.querySelector(".conteneur-slides");
+let slides = conteneur.querySelectorAll(".slides");
 
-function afficherSlide(n) {
-    for (var i = 0; i < slides.length; i++) {
-        slides[i].classList.remove("active");
-    }
-
-    slideIndex += n;
-
-    if (slideIndex > slides.length - 1) {
-        slideIndex = 0;
-    }
-
-    if (slideIndex < 0) {
-        slideIndex = slides.length - 1;
-    }
-
-    slides[slideIndex].classList.add("active");
-}
+changerSlide(slideIndex);
 
 function changerSlide(n) {
+
+    function afficherSlide(n) {
+
+        for (var i = 0; i < slides.length; i++) {
+            slides[i].classList.remove("active");
+        }
+
+        slideIndex += n;    
+
+        if (slideIndex > slides.length - 1) {
+            slideIndex = 0;
+        }
+
+        if (slideIndex < 0) {
+            slideIndex = slides.length - 1;
+        }
+        
+
+        slides[slideIndex].classList.add("active");
+        }
+
     afficherSlide(n);
 }
 
-afficherSlide(slideIndex);
+
+
+//tableau fetch
+
+//incremente et decremente avec bouton
+
+//variable counter
+
+//fonction generer les tarifs et soins
+
+//recup donnes sur accueil
+
+//fonction pour generer html 
+
+//quey du formulaire
+// evenement submit
+//fetch du submit
