c6549358605d9c05652ff146da51805f55a55bc4
BahiaL <bahlarcade@hotmail.com>
2023-03-29 14:53:41 +0200

    debut fetch avis

--- prod/script.js
+++ prod/script.js
@@ -1,10 +1,12 @@
+//carroussel
+
 let slideIndex = 0;
 let conteneur = document.querySelector(".conteneur-slides");
 let slides = conteneur.querySelectorAll(".slides");
 
 changerSlide(slideIndex);
 
-function changerSlide(n) {
+function changerSlide(n, conteneur) {
 
     function afficherSlide(n) {
 
@@ -29,6 +31,36 @@ function changerSlide(n) {
     afficherSlide(n);
 }
 
+//fetch les avis
+let formulaire = document.querySelector("form")
+let divAvis = document.querySelector(".avis");
+
+formulaire.addEventListener("submit", function(event) {
+    event.preventDefault(); 
+
+    const formData = new FormData();
+    formData.append("prenom", prenom);
+    formData.append("msg", msg);
+
+    fetch("http://localhost/raiponce-coiffure/prod/", {
+        method: "POST",
+        body: formData
+        })
+        .then(response => response.json())
+        .then(data => {
+            let avis = "";
+            avis += `
+                <p>${prenom}</p>
+                <p>${msg}</p>                
+                `;
+
+            divAvis.innerHTML = avis;
+            console.log(data);
+        })
+        .catch(error => {
+            console.error(error);
+        });
+});
 
 
 //tableau fetch
